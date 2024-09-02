3968e8e414372adb1876f516c18a5da11a2f5d4e
Nick Brown <nworbn@gmail.com>
2024-09-02 10:25:55 -0700

    more basic stuff

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Fonts/TomThumb.h>
 
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <WiFi.h>
+
+#include "secrets.h"
+
 #define PIN A3
+
+WebServer restServer(80);
 
 TaskHandle_t matrix5x5TaskHandle = NULL;
 TaskHandle_t matrix9x16TaskHandle = NULL;
@@ -20,13 +29,13 @@ Adafruit_NeoMatrix matrix5x5(5, 5, PIN,
                                  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
                              NEO_GRB + NEO_KHZ800);
 
-const char message[] = "BEAU AND STELLA";
+char* matrix5x5Message;
 const uint16_t colors[] = {
     matrix5x5.Color(255, 60, 0), matrix5x5.Color(255, 120, 0), matrix5x5.Color(255, 255, 0)};
 uint16_t message_width;     // Computed in setup() below
-int x = matrix5x5.width();  // Start with message off right edge
-int y = matrix5x5.height(); // With custom fonts, y is the baseline, not top
-int pass = 0;               // Counts through the colors[] array
+int matrix5x5_x = matrix5x5.width();  // Start with message off right edge
+int matrix5x5_y = matrix5x5.height(); // With custom fonts, y is the baseline, not top
+int matrix5x5_pass = 0;               // Counts through the colors[] array
 ///////////////
 
 ///////////////
@@ -42,6 +51,8 @@ int text_y = 1;
 int text_min; // Pos. where text resets (calc'd later)
 uint16_t hue_offset = 0;
 ///////////////
+
+
 
 ///////////////
 #define LED_COUNT 64
@@ -63,18 +74,39 @@ void Matrix5x5Task(void *parameters)
   while (1)
   {
     matrix5x5.fillScreen(0);   // Erase message in old position.
-    matrix5x5.setCursor(x, y); // Set new cursor position,
-    matrix5x5.print(message);  // draw the message
+    matrix5x5.setCursor(matrix5x5_x, matrix5x5_y); // Set new cursor position,
+    matrix5x5.print(matrix5x5Message);  // draw the message
     matrix5x5.show();          // and update the matrix.
-    if (--x < -message_width)
+    if (--matrix5x5_x < -message_width)
     {                        // Move 1 pixel left. Then, if scrolled off left...
-      x = matrix5x5.width(); // reset position off right edge and
-      if (++pass >= 3)
-        pass = 0; // increment color in list, rolling over if needed.
-      matrix5x5.setTextColor(colors[pass]);
+      matrix5x5_x = matrix5x5.width(); // reset position off right edge and
+      if (++matrix5x5_pass >= 3)
+        matrix5x5_pass = 0; // increment color in list, rolling over if needed.
+      matrix5x5.setTextColor(colors[matrix5x5_pass]);
     }
     delay(100); // 1/10 sec pause
   }
+}
+
+void Matrix5x5Setup()
+{
+  matrix5x5Message = new char[100];
+  strcpy(matrix5x5Message, "BEAU IN TOW!");
+
+  matrix5x5.begin();
+  matrix5x5.setBrightness(40);       // Turn down brightness to about 15%
+  matrix5x5.setFont(&TomThumb);      // Use custom font
+  matrix5x5.setTextWrap(false);      // Allows text to scroll off edges
+  matrix5x5.setTextColor(colors[0]); // Start with first color in list
+  // To determine when the message has fully scrolled off the left side,
+  // get the bounding rectangle of the text. As we only need the width
+  // value, a couple of throwaway variables are passed to the bounds
+  // function for the other values:
+  int16_t d1;
+  uint16_t d2;
+  matrix5x5.getTextBounds(matrix5x5Message, 0, 0, &d1, &d1, &message_width, &d2);
+
+  xTaskCreate(Matrix5x5Task, "Matrix5x5Task", 4096, NULL, 10, &matrix5x5TaskHandle);
 }
 
 void Matrix9x16Task(void *parameters)
@@ -87,6 +119,19 @@ void Matrix9x16Task(void *parameters)
           matrix9x16.drawPixel(x, y, sweep[(x + y + incr) % 24]);
     delay(20);
   }
+}
+
+void Matrix9x16Setup()
+{
+  if (!matrix9x16.begin())
+  {
+    Serial.println("IS31 not found");
+    while (1)
+      ;
+  }
+  Serial.println("IS31 found!");
+
+  xTaskCreate(Matrix9x16Task, "Matrix9x16Task", 4096, NULL, 10, &matrix9x16TaskHandle);
 }
 
 void Matrix9x13Task(void *parameters)
@@ -128,51 +173,8 @@ void Matrix9x13Task(void *parameters)
   }
 }
 
-void Matrix8x8Task(void *parameters)
+void Matrix9x13Setup()
 {
-  while (1)
-  {
-    for (int i = 0; i < LED_COUNT; i++)
-    {
-      if (--pixelData[i].delay > 0)
-        continue;
-
-      pixelData[i].color = pixelData[i].color == LED_OFF ? LED_YELLOW : LED_OFF;
-      pixelData[i].delay = random(BASEDELAY, BASEDELAY * 2);
-
-      matrix8x8.drawPixel(i / 8, i % 8, pixelData[i].color);
-    }
-
-    matrix8x8.writeDisplay();
-    delay(REFRESHTIME);
-  }
-}
-
-void setup()
-{
-  Wire.setClock(1000000);
-
-  matrix5x5.begin();
-  matrix5x5.setBrightness(40);       // Turn down brightness to about 15%
-  matrix5x5.setFont(&TomThumb);      // Use custom font
-  matrix5x5.setTextWrap(false);      // Allows text to scroll off edges
-  matrix5x5.setTextColor(colors[0]); // Start with first color in list
-  // To determine when the message has fully scrolled off the left side,
-  // get the bounding rectangle of the text. As we only need the width
-  // value, a couple of throwaway variables are passed to the bounds
-  // function for the other values:
-  int16_t d1;
-  uint16_t d2;
-  matrix5x5.getTextBounds(message, 0, 0, &d1, &d1, &message_width, &d2);
-
-  if (!matrix9x16.begin())
-  {
-    Serial.println("IS31 not found");
-    while (1)
-      ;
-  }
-  Serial.println("IS31 found!");
-
   if (!matrix9x13.begin(IS3741_ADDR_DEFAULT))
   {
     Serial.println("IS41 not found");
@@ -195,6 +197,31 @@ void setup()
   matrix9x13.getTextBounds(text, 0, 0, &ignore, &ignore, &w, &h);
   text_min = -w; // Off left edge this many pixels
 
+  xTaskCreate(Matrix9x13Task, "Matrix9x13Task", 4096, NULL, 10, &matrix9x13TaskHandle);
+}
+
+void Matrix8x8Task(void *parameters)
+{
+  while (1)
+  {
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+      if (--pixelData[i].delay > 0)
+        continue;
+
+      pixelData[i].color = pixelData[i].color == LED_OFF ? LED_YELLOW : LED_OFF;
+      pixelData[i].delay = random(BASEDELAY, BASEDELAY * 2);
+
+      matrix8x8.drawPixel(i / 8, i % 8, pixelData[i].color);
+    }
+
+    matrix8x8.writeDisplay();
+    delay(REFRESHTIME);
+  }
+}
+
+void Matrix8x8Setup()
+{
   matrix8x8.begin(0x70);
   matrix8x8.setBrightness(5);
 
@@ -208,12 +235,158 @@ void setup()
     matrix8x8.drawPixel(i / 8, i % 8, pixelData[i].color);
   }
 
-  xTaskCreate(Matrix5x5Task, "Matrix5x5Task", 4096, NULL, 10, &matrix5x5TaskHandle);
-  xTaskCreate(Matrix9x16Task, "Matrix9x16Task", 4096, NULL, 10, &matrix9x16TaskHandle);
-  xTaskCreate(Matrix9x13Task, "Matrix9x13Task", 4096, NULL, 10, &matrix9x13TaskHandle);
   xTaskCreate(Matrix8x8Task, "Matrix8x8Task", 4096, NULL, 10, &matrix8x8TaskHandle);
+}
+
+void otaSetup()
+{
+  Serial.println("OTA setting up...");
+
+  // ArduinoOTA.setPort(3232);
+  ArduinoOTA.setHostname("I2C-LED-Controller");
+  ArduinoOTA.setPasswordHash("d5aa78e4cc65133dffb98c32605ff9d1"); // password
+
+  ArduinoOTA
+      .onStart([]()
+               {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else  // U_SPIFFS
+          type = "filesystem";
+
+        Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+          Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+          Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+          Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+          Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR)
+          Serial.println("End Failed"); });
+  ArduinoOTA.begin();
+
+
+  Serial.println("OTA setup complete.");
+}
+
+void mDnsSetup()
+{
+  if (!MDNS.begin("i2c-led-controller"))
+  {
+    Serial.println("Error setting up MDNS responder!");
+
+    return;
+  }
+  Serial.println("mDNS responder started");
+
+  // TODO: OTA service?
+  //MDNS.addService("http", "tcp", 80);
+}
+
+void wifiSetup()
+{
+  Serial.println("Wifi setting up...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  Serial.print("Wifi ready, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+volatile long wifiStatusDelayMs = 0;
+int wifiDisconnects = 0;
+void checkWifiStatus()
+{
+  if (wifiStatusDelayMs < 0)
+  {
+    try
+    {
+      if (WiFi.status() != WL_CONNECTED)
+      {
+        Serial.println("Reconnecting to WiFi...");
+        WiFi.disconnect();
+        WiFi.reconnect();
+        wifiDisconnects++;
+        Serial.println("Reconnected to WiFi");
+      }
+    }
+    catch (const std::exception &e)
+    {
+      Serial.println("Wifi error:" + String(e.what()));
+      wifiStatusDelayMs = 10 * 60 * 1000; // 10 minutes
+    }
+
+    wifiStatusDelayMs = 60 * 1000; // 1 minute
+  }
+}
+
+void restIndex()
+{
+  Serial.println("Serving index.html");
+  restServer.send(200, "text/plain", "test");
+  Serial.println("Served index.html");
+}
+
+void restSet5x5MatrixMessage()
+{
+  if (!restServer.hasArg("messasge"))
+  {
+    restServer.send(400, "text/plain", "No message provided");
+    Serial.println("No message provided");
+    return;
+  }
+
+  String message = restServer.arg("value");
+  message = message.substring(0, 100);
+  strcpy(matrix5x5Message, message.c_str());
+}
+
+void restSetup() {
+  restServer.on("/", HTTP_GET, restIndex);
+  restServer.on("/5x5", HTTP_GET, restSet5x5MatrixMessage);
+  restServer.begin();
+
+  Serial.println("REST server running");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting setup...");
+
+  wifiSetup();
+  mDnsSetup();
+  otaSetup();
+  restSetup();
+
+  Wire.setClock(1000000);
+
+  Matrix5x5Setup();
+  Matrix9x16Setup();
+  Matrix9x13Setup();
+  Matrix8x8Setup();
 }
 
 void loop()
 {
+  // TOOD: wifi status?
+  ArduinoOTA.handle();
+  restServer.handleClient();
+  checkWifiStatus();
 }
