fa2558e2bc1b3b9d0b8e68bc94fdb146796e6533
Nick Brown <nworbn@gmail.com>
2024-10-19 12:06:47 -0700

    refactor

--- matrix_16x9.h
+++ matrix_16x9.h
@@ -10,17 +10,18 @@ extern volatile bool display;
 class Matrix16x9TaskHandler : public MatrixTaskHandler
 {
 private:
-  Adafruit_IS31FL3731 matrix = Adafruit_IS31FL3731();
-  static const uint8_t DISPLAY_WIDTH = 16;
-  static const uint8_t DISPLAY_HEIGHT = 9;
+  static const uint8_t WIDTH = 16;
+  static const uint8_t HEIGHT = 9;
   static constexpr const char *TAG = "Matrix16x9TaskHandler";
-  char message[100];
-  bool messagePixels[DISPLAY_HEIGHT][DISPLAY_WIDTH];
+  static const uint8_t ScanWidth = 7;
+  
+  uint8_t ScanVals[7] = {120, 40, 20, 5, 2, 1, 0};
   // uint8_t sweep[] = {1, 2, 3, 4, 6, 8, 10, 15, 20, 30, 40, 60, 60, 40, 30, 20, 15, 10, 8, 6, 4, 3, 2, 1};
-  uint8_t scan[7] = {120, 40, 20, 5, 2, 1, 0};
-  int scanX = -1;
-  int direction = 1;
-  uint8_t scanWidth = 7;
+  
+  Adafruit_IS31FL3731 _matrix = Adafruit_IS31FL3731();
+  bool _messagePixels[HEIGHT][WIDTH];
+  int _direction = 1;
+  int _scanX = -1;
 
 public:
   Matrix16x9TaskHandler() {}
@@ -28,6 +29,9 @@ public:
 
 private:
   void matrixTask(void *parameters) override;
+
+  void initializeMessage();
+  void setCharPixels(int16_t x, int16_t y, char c, uint8_t &glyphWidth);
 };
 
 
@@ -39,8 +43,8 @@ bool Matrix16x9TaskHandler::createTask()
         return false;
     }
 
-    strcpy(message, "ABC");
-    if (!matrix.begin())
+    strcpy(_message, "ABC");
+    if (!_matrix.begin())
     {
         log_e("Matrix not found");
         return false;
@@ -63,42 +67,42 @@ void Matrix16x9TaskHandler::matrixTask(void *parameters)
     {
         if (!display)
         {
-            matrix.clear();
+            _matrix.clear();
             delay(100);
             continue;
         }
 
-        if (direction > 0 && scanX >= DISPLAY_WIDTH + scanWidth)
+        if (_direction > 0 && _scanX >= WIDTH + ScanWidth)
         {
-            scanX = DISPLAY_WIDTH;
-            direction = -1;
+            _scanX = WIDTH;
+            _direction = -1;
         }
-        else if (direction < 0 && scanX <= -scanWidth)
+        else if (_direction < 0 && _scanX <= -ScanWidth)
         {
-            scanX = -1;
-            direction = 1;
+            _scanX = -1;
+            _direction = 1;
         }
 
-        for (int scanIndex = 0; scanIndex < scanWidth; scanIndex++)
+        for (int scanIndex = 0; scanIndex < ScanWidth; scanIndex++)
         {
-            int x = scanX - (direction * scanIndex);
-            if (x < 0 || x >= DISPLAY_WIDTH)
+            int x = _scanX - (_direction * scanIndex);
+            if (x < 0 || x >= WIDTH)
             {
                 continue;
             }
 
-            for (int y = 0; y < DISPLAY_HEIGHT; y++)
+            for (int y = 0; y < HEIGHT; y++)
             {
                 // matrix16x9.drawPixel(x, y, scan[scanIndex]);
-                uint16_t brightness = scan[scanIndex];
-                if (messagePixels[y][x])
+                uint16_t brightness = ScanVals[scanIndex];
+                if (_messagePixels[y][x])
                 {
                     brightness = brightness > 0 ? 150 : 0;
                 }
-                matrix.drawPixel(x, y, brightness);
+                _matrix.drawPixel(x, y, brightness);
             }
         }
-        scanX += direction;
+        _scanX += _direction;
 
         // for (uint8_t incr = 0; incr < 24; incr++)
         //   for (uint8_t x = 0; x < 16; x++)
@@ -108,63 +112,57 @@ void Matrix16x9TaskHandler::matrixTask(void *parameters)
     }
 }
 
-// void Matrix16x9TaskHandler::setCharPixels(int16_t x, int16_t y, char c, uint8_t &glyphWidth)
-// {
-//   c -= (uint8_t)pgm_read_byte(&Font3x4N.first); // Adjust the character index
+void Matrix16x9TaskHandler::initializeMessage()
+{
+  memset(_messagePixels, 0, sizeof(_messagePixels));
 
-//   GFXglyph *glyph = &(((GFXglyph *)pgm_read_ptr(&Font3x4N.glyph))[c]);
-//   uint8_t *bitmap = (uint8_t *)pgm_read_ptr(&Font3x4N.bitmap);
+  // iterate over the message and set the pixels
+  int cursorX = 1;
+  int cursorY = 1;
+  String message = String(_message);
+  for (int i = 0; i < message.length(); i++)
+  {
+    uint8_t charWidth;
+    setCharPixels(cursorX, cursorY, message[i], charWidth);
+    cursorX += charWidth;
+  }
+}
 
-//   uint16_t bo = pgm_read_word(&glyph->bitmapOffset);
-//   uint8_t w = pgm_read_byte(&glyph->width);
-//   uint8_t h = pgm_read_byte(&glyph->height);
-//   int8_t xo = pgm_read_byte(&glyph->xOffset);
-//   int8_t yo = pgm_read_byte(&glyph->yOffset);
-//   uint8_t xx, yy, bits = 0, bit = 0;
+void Matrix16x9TaskHandler::setCharPixels(int16_t x, int16_t y, char c, uint8_t &glyphWidth)
+{
+  c -= (uint8_t)pgm_read_byte(&Font3x4N.first); // Adjust the character index
 
-//   for (yy = 0; yy < h; yy++)
-//   {
-//     for (xx = 0; xx < w; xx++)
-//     {
-//       if (!(bit++ & 7))
-//       {
-//         bits = pgm_read_byte(&bitmap[bo++]);
-//       }
-//       if (bits & 0x80)
-//       {
-//         int x = x + xo + xx;
-//         int y = y + yo + yy;
-//         if (x >= 0 && x < DISPLAY_WIDTH && y >= 0 && y < DISPLAY_HEIGHT)
-//         {
-//           messagePixels[y][x] = true;
-//         }
-//       }
-//       bits <<= 1;
-//     }
-//   }
+  GFXglyph *glyph = &(((GFXglyph *)pgm_read_ptr(&Font3x4N.glyph))[c]);
+  uint8_t *bitmap = (uint8_t *)pgm_read_ptr(&Font3x4N.bitmap);
 
-//   glyphWidth = w;
-// }
+  uint16_t bo = pgm_read_word(&glyph->bitmapOffset);
+  uint8_t w = pgm_read_byte(&glyph->width);
+  uint8_t h = pgm_read_byte(&glyph->height);
+  int8_t xo = pgm_read_byte(&glyph->xOffset);
+  int8_t yo = pgm_read_byte(&glyph->yOffset);
+  uint8_t xx, yy, bits = 0, bit = 0;
 
-// void Matrix16x9TaskHandler::initializeMessage()
-// {
-//   memset(messagePixels, 0, sizeof(messagePixels));
-//   // for (int y = 0; y < DISPLAY_HEIGHT; y++)
-//   // {
-//   //   for (int x = 0; x < DISPLAY_WIDTH; x++)
-//   //   {
-//   //     messagePixels[y][x] = false;
-//   //   }
-//   // }
+  for (yy = 0; yy < h; yy++)
+  {
+    for (xx = 0; xx < w; xx++)
+    {
+      if (!(bit++ & 7))
+      {
+        bits = pgm_read_byte(&bitmap[bo++]);
+      }
+      if (bits & 0x80)
+      {
+        int x = x + xo + xx;
+        int y = y + yo + yy;
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+        {
+          _messagePixels[y][x] = true;
+        }
+      }
+      bits <<= 1;
+    }
+  }
 
-//   // iterate over the message and set the pixels
-//   int cursorX = 1;
-//   int cursorY = 1;
-//   String message = String(matrix16x9Message);
-//   for (int i = 0; i < message.length(); i++)
-//   {
-//     uint8_t charWidth;
-//     setCharPixels(cursorX, cursorY, message[i], charWidth);
-//     cursorX += charWidth;
-//   }
-// }
+  glyphWidth = w;
+}
+
